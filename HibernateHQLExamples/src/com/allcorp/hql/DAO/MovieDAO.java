7884c195fd055381ea03f195aa96e534c9858090
Admin <Admin@DESKTOP-011VR2B>
2019-09-09 23:31:48 +0530

    First HQl query example

---
+++ HibernateHQLExamples/src/com/allcorp/hql/DAO/MovieDAO.java
@@ -0,0 +1,47 @@
+package com.allcorp.hql.DAO;
+
+import java.io.Serializable;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.allcorp.hibernate.Util.HibernateUtil;
+import com.allcorp.hql.DTO.MovieDTO;
+
+public class MovieDAO {
+	public int save(MovieDTO dto) {
+		Session session=HibernateUtil.getFactory().openSession();
+		Transaction tx=session.beginTransaction();
+		Serializable pk=null;
+		try {
+			pk=session.save(dto);
+			System.out.println("Pk generated \t "+pk);
+			tx.commit();
+		}catch(HibernateException he) {
+			he.printStackTrace();
+			tx.rollback();
+		}finally {
+			session.close();
+		}
+		return (int) pk;
+
+	}
+	public MovieDTO getByName(String name) {
+		Session session=HibernateUtil.getFactory().openSession();
+		MovieDTO fromDb = null;
+		try {
+		String query="From MovieDTO where "+ "m_name='"+name+"'";
+		Query hql=session.createQuery(query);
+		fromDb=(MovieDTO) hql.uniqueResult();
+		return fromDb;
+		}catch(HibernateException he) {
+			he.printStackTrace();
+		}
+		finally {
+			session.close();
+		}
+		return fromDb;
+	}
+}
---
+++ HibernateHQLExamples/src/com/allcorp/hql/Util/MovieTester.java
@@ -0,0 +1,17 @@
+package com.allcorp.hql.Util;
+
+import com.allcorp.hql.DAO.MovieDAO;
+import com.allcorp.hql.DTO.MovieDTO;
+
+public class MovieTester {
+	public static void main(String[] args) {
+		MovieDAO dao=new MovieDAO();
+		MovieDTO singham2=new MovieDTO();
+		  singham2.setCertificate("U/A"); 
+		  singham2.setDuration(126);
+		  singham2.setName("Singham 2");
+		dao.save(singham2);
+		MovieDTO movie=dao.getByName("Singham 2");
+		System.out.println(movie);// override toString method in MovieDTO.
+	}
+}
