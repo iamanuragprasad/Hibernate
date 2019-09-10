341670bb2defb1435eee7cba4c60d9976439b373
Admin <Admin@DESKTOP-011VR2B>
2019-09-10 22:57:14 +0530

    Enhancememts

--- HibernateHQLExamples/src/com/allcorp/hql/DAO/MovieDAO.java
+++ HibernateHQLExamples/src/com/allcorp/hql/DAO/MovieDAO.java
@@ -32,9 +32,14 @@ public class MovieDAO {
 		Session session=HibernateUtil.getFactory().openSession();
 		MovieDTO fromDb = null;
 		try {
-		String query="From MovieDTO where "+ "m_name='"+name+"'"; // Here m_name is column name within DB.
-		// Notice here we are using certificate and name( class properties) instead of column name as HQL uses class properties.
-		String query1="select certificate from MovieDTO where name='Singham 2'"; // Notice here we are using certificate and name( class properties>
+			/*
+			 * Below m_name is column name within DB. Don't use column name in HQL as HQL
+			 * uses class property. Use the one below.
+			 */		
+		//String query="From MovieDTO where "+ "m_name='"+name+"'"; 
+		String query="From MovieDTO where "+"name='"+name+"'"; //Here name is a property in  MovieDTO
+		// Notice here below we are using certificate and name( class properties) instead of column name as HQL uses class properties.
+		String query1="select certificate from MovieDTO where name='Singham 2'"; // Notice here we are using certificate and name( class properties)
 		Query hql1=session.createQuery(query);
 		Query hql=session.createQuery(query1);
 		fromDb= (MovieDTO) hql1.uniqueResult();
